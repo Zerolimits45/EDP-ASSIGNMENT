4bc7cdb85fc1cfd3e0664efde3d6c1b6198221c3
MiloMuncher <116133512+MiloMuncher@users.noreply.github.com>
2024-01-11 14:52:34 +0800

    view post

---
+++ client/src/Pages/ViewPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Container, Box, Paper, Grid, Typography, Button, Divider, Card, CardContent } from '@mui/material'
+
+function ViewPost(){
+    return(
+        <div>About Us</div>
+    )
+}
+export default ViewPost
